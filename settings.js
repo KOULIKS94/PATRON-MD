6180576fe94465a9781af5972a964232224b24ed
KOULIKS94 <senguptakoulik94@gmail.com>
2025-01-07 13:53:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 global.bankname = "KAO"
 
-global.banknumber = "8293316342"
+global.banknumber = "8250919114"
 
 global.bankowner = "PATRON"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.wlcm = true
+global.wlcm = false 
 global.autoblockmorroco = true //auto block 212 (true to on, false to off)
 global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
 global.antispam = false//auto kick spammer (true to on, false to off)
